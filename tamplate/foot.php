a6883d4f8e3949cc3be1e827e360665a108b0321
karndevza <karnkung30@gmail.com>
2020-07-08 11:45:31 +0700

    Create foot.php

---
+++ tamplate/foot.php
@@ -0,0 +1,36 @@
+<!--  End  -->
+<footer class="footer" data-background-color="black">
+    <div class="container">
+      <nav class="float-left">
+        <ul>
+          <li>
+            <a href="#">
+              SMSGOLDTHAI
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Licenses
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright float-right">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="#" target="_blank">Karn Diy</a> 
+      </div>
+    </div>
+  </footer>
