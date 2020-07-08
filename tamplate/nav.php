ca288d9865501949bd64bcff997480450aac6ff4
karndevza <karnkung30@gmail.com>
2020-07-08 11:45:35 +0700

    Create nav.php

---
+++ tamplate/nav.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top  navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
+          ราคาทองคำวันนี้ </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">apps</i> เมนู
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="./index.html" class="dropdown-item">
+                <i class="material-icons">layers</i> ราคาทองคำวันนี้
+              </a>
+              <a href="#" 
+                 class="dropdown-item">
+                <i class="material-icons">content_paste</i> สรุปราคาทองคำ
+              </a>
+            </div>
+          </li>        
+     
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" 
+             href="https://twitter.com/smsgoldthai" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" 
+            href="https://twitter.com/smsgoldthai" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+              <i class="fa fa-youtube"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" 
+            href="https://www.facebook.com/smsgoldthai" target="_blank" data-original-title="Like us on Facebook" rel="nofollow">
+              <i class="fa fa-facebook-square"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" 
+            href="https://www.instagram.com/smsgoldthai" target="_blank" data-original-title="Follow us on Instagram" rel="nofollow">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" 
+            style="background-image: url('./assets/img/bg2.jpg');">    
+    </div>  
